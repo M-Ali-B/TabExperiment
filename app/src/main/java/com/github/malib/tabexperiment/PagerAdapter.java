240ac8a1b0de4d573c9487bbcc7022b55f562f4b
Ali Baba <alibabatorjan001@gmail.com>
2019-05-24 20:54:28 +0500

    Initial commit

---
+++ app/src/main/java/com/github/malib/tabexperiment/PagerAdapter.java
@@ -0,0 +1,31 @@
+package com.github.malib.tabexperiment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNumofTabs;
+
+    public PagerAdapter(FragmentManager fm,int numofTabs) {
+        super(fm);
+        this.mNumofTabs = numofTabs;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0: return  new TabFragment();
+            case 1: return  new TabFragment2();
+            case 2: return  new TabFragment3();
+            default:return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return mNumofTabs;
+    }
+}
